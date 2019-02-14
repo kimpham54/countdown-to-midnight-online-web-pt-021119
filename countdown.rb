20d5e2af9d446ce1df0f936152a3ba4e65c81685
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-02-14 02:48:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 counter = 11
 
-def countdown(seconds)
+def countdown(counter)
   while counter > 0
   puts "#{seconds} SECOND(S)!"
   counter -= 1
@@ -9,7 +9,7 @@ def countdown(seconds)
 end
 
 
-def countdown_with_sleep(seconds)
+def countdown_with_sleep(counter)
   while counter > 0
   sleep(1.0)
   puts "#{seconds} SECOND(S)!"
